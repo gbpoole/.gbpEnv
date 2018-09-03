f20bcc561d76f0a0a172ca2d961d4e379bc9ccb4
Gregory Poole <gbpoole@gmail.com>
2018-09-04 01:20:28 +1000

    Fixed travis tests, broken by last commit.

--- .test/assert_bash_config.sh
+++ .test/assert_bash_config.sh
@@ -4,11 +4,11 @@
 
 # Sourcing the following files should return 0
 assert_raises "source .bashrc" 0
-assert_raises "source .alias.bash.gbpHome" 0
+assert_raises "source .bash.alias" 0
 
 # ... they should also output nothing ...
 assert "source .bashrc 2>&1" ""
-assert "source .alias.bash.gbpHome 2>&1" ""
+assert "source .bash.alias 2>&1" ""
 
 # end of test suite
 assert_end examples
