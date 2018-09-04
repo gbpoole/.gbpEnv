f263cb3a9a2b235c0cf71ba0c397dbc5d1abad83
Gregory Poole <gbpoole@gmail.com>
2018-09-04 16:09:51 +1000

    Fixed a bug in the previous commit.

--- .test/assert_bash_config.sh
+++ .test/assert_bash_config.sh
@@ -4,11 +4,11 @@
 
 # Sourcing the following files should return 0
 assert_raises "source .bashrc" 0
-assert_raises "source .bash.alias" 0
+assert_raises "source .bashrc.alias" 0
 
 # ... they should also output nothing ...
 assert "source .bashrc 2>&1" ""
-assert "source .bash.alias 2>&1" ""
+assert "source .bashrc.alias 2>&1" ""
 
 # end of test suite
 assert_end examples
