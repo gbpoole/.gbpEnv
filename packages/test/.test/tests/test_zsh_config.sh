1e18f5365a5b0424e2b8302d5c4a66273e5be8f3
Gregory Poole <gbpoole@gmail.com>
2019-03-11 22:12:04 +1100

    Fixed a bug where zsh tests weren't being turned off for Travis.

--- packages/test/.test/tests/test_zsh_config.sh
+++ packages/test/.test/tests/test_zsh_config.sh
@@ -4,14 +4,14 @@
 
 # Sourcing the following files should return 0
 # Note that .zshrc needs executable permissions for this
-if [ ! -n "${GBP_TEST}" ]; then
+if [ ! -n "${GBPENV_TEST}" ]; then
     assert_raises "./.zshrc" 0  # Turned-off for Travis becuase I couldn't figure-out how to install zsh adequately
 fi
 
 # ... they should also output nothing ...
 # The version of the pure shell we are using generates
 # 4 spurious carriage returns when run as a non-login shell
-if [ ! -n "${GBP_TEST}" ]; then
+if [ ! -n "${GBPENV_TEST}" ]; then
     assert "./.zshrc 2>&1" "\n\n\n\n" # Turned-off for Travis becuase I couldn't figure-out how to install zsh adequately
 fi
 
