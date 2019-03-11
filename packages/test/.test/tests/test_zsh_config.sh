2d5b0aacaa5174e4b40c61305c2083d9269593f0
Gregory Poole <gbpoole@gmail.com>
2019-03-11 21:04:31 +1100

    Debugging of .travis.yml

--- packages/test/.test/tests/test_zsh_config.sh
+++ packages/test/.test/tests/test_zsh_config.sh
@@ -9,7 +9,7 @@ assert_raises "./.zshrc" 0
 # ... they should also output nothing ...
 # The version of the pure shell we are using generates
 # 4 spurious carriage returns when run as a non-login shell
-#assert "./.zshrc 2>&1" ""
+assert "./.zshrc 2>&1" ""
 
 # end of test suite
 assert_end zsh_config_tests
