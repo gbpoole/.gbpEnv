a3190b5eb84faa88bbad58aeb5884edaaca594b1
Gregory Poole <gbpoole@gmail.com>
2019-03-13 11:43:44 +1100

    Renamed git.commit to git.c and tweeked it a bit.

---
+++ packages/bin/bin/autoload/git.c
@@ -0,0 +1,37 @@
+# vim:syntax=sh
+# vim:filetype=sh
+
+# --------------
+# Validate input
+if [ ! $# -eq 0 ]; then
+    # This does not preserve single-quotes
+    #COMMIT_MESSAGE="$@"
+    # This comes from here: https://unix.stackexchange.com/questions/197792/joining-bash-arguments-into-single-string-with-spaces
+    COMMIT_MESSAGE="'$*'"
+else
+    COMMIT_MESSAGE=""
+fi
+if [ -z "$COMMIT_MESSAGE" ]; then
+    echo 'Syntax:' $0 'text for commit message'
+    return 2
+fi
+
+# Stage (upon validation)
+git status
+gbp_user_validate "Stage changes"
+if [ $? -eq 0 ] ; then
+    return 1
+fi
+git add .
+
+# Commit (upon validation)
+git status
+gbp_user_validate "Commit with message {$COMMIT_MESSAGE}"
+if [ $? -eq 0 ] ; then
+    return 1
+fi
+git commit -m "$COMMIT_MESSAGE"
+
+# TODO: Merge with remote (upon validation)
+# See this for some tips: https://stackoverflow.com/questions/17719829/check-if-local-git-repo-is-ahead-behind-remote
+#gbp_user_validate "Merge with remote"
